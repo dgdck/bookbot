d1c90d0951dec3f17dfd3aa980f5d983fdb8cda6
dgdck <d.godecke@gmail.com>
2025-12-23 13:39:07 +0000

    count words complete

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ def get_book_text(filepath):
         file_contents = f.read()
     
     return file_contents
+
+
+def count_words(file_contents):
+    total_words = len(file_contents.split())
+    return f'Found {total_words} total words'
+
+
+def main():
+    print(
+        count_words(
+            get_book_text("books/frankenstein.txt")
+        )
+    )
+
+
+main()
